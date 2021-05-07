6fd18b9f846ad9a8573c743e6c6bbabd381f99ca
Rishabh <sunny@Sunnys-MacBook-Air.local>
2021-05-07 18:51:32 +0530

    Till Snackbar

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -2,25 +2,38 @@ package com.example.happybirthday
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.CheckBox
-import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //checkStateOfCheckBox()
+        Toast.makeText(applicationContext,"Application is Started",Toast.LENGTH_SHORT).show()
+//        val pressButton = findViewById<View>(R.id.press) as Button
+//        pressButton.setOnClickListener {
+//            print("button pressed");
+//        }
+//        checkStateOfCheckBox()
     }
 
+    fun buttonPressed(v :View){
+        println("button pressed from UI call")
+        val btn = Toast.makeText(applicationContext,"Button Pressed",Toast.LENGTH_SHORT)
+        btn.show();
+        Snackbar.make(v,"Basic SnackBar",4000).setAction("Yes",View.OnClickListener {
+            Toast.makeText(applicationContext,"Dismiss SnackBar",Toast.LENGTH_SHORT).show()
+        }).show()
+    }
+    fun imagePressed(v :View){
+        println("image pressed from UI call")
+        val img = Toast.makeText(applicationContext,"Image Pressed",Toast.LENGTH_SHORT)
+        img.show()
+    }
     fun checkStateOfCheckBox (){
-        val myButton = Button(this)
-        myButton.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-        )
         val checkBox = findViewById<View>(android.R.id.checkbox) as CheckBox
         val checkBoxState = checkBox.isChecked
         print(checkBoxState)
