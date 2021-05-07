8a5df7fe2d5496822aa2365625368cf116ee7e7d
Rishabh <sunny@Sunnys-MacBook-Air.local>
2021-05-07 12:10:53 +0530

    till radio button and check box

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.happybirthday
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,12 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myButton = Button(this)
-        myButton.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.MATCH_PARENT
-        )
-
+        //checkStateOfCheckBox()
     }
 
+    fun checkStateOfCheckBox (){
+        val myButton = Button(this)
+        myButton.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        val checkBox = findViewById<View>(android.R.id.checkbox) as CheckBox
+        val checkBoxState = checkBox.isChecked
+        print(checkBoxState)
+    }
 }
