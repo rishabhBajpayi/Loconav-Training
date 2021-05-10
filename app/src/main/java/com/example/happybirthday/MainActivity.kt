4dc2687f14b34b226d2ec54d7e564d9189b4957b
Rishabh <sunny@Sunnys-MacBook-Air.local>
2021-05-10 17:29:20 +0530

    Dice app done

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import android.text.Html
 import android.view.View
-import android.widget.CheckBox
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
@@ -13,28 +11,56 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.hb_activity_main)
-        Toast.makeText(applicationContext,"Application is Started",Toast.LENGTH_SHORT).show()
+//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.hb_activity_main)
+        setContentView(R.layout.dice_activity_main)
+        Toast.makeText(applicationContext, "Application is Started", Toast.LENGTH_SHORT).show()
+        val btn: Button = findViewById(R.id.rollBtn)
+        btn.setOnClickListener {
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            roll()
+        }
 //        val txt = findViewById<TextView>(R.id.message)
 //        txt.setText(Html.fromHtml("<h2>Title</h2><br><p>Description here</p>"))
     }
 
-    fun buttonPressed(v :View){
-        println("button pressed from UI call")
-        val btn = Toast.makeText(applicationContext,"Button Pressed",Toast.LENGTH_SHORT)
-        btn.show();
-        Snackbar.make(v,"Basic SnackBar",4000).setAction("Yes",View.OnClickListener {
-            Toast.makeText(applicationContext,"Dismiss SnackBar",Toast.LENGTH_SHORT).show()
-        }).show()
+    private fun roll() {
+        val d = Dice(6).rollDice()
+        val draw = when (d) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val img: ImageView = findViewById(R.id.rollResult)
+        img.setImageResource(draw)
+        img.contentDescription = d.toString()
     }
-    fun imagePressed(v :View){
-        println("image pressed from UI call")
-        val img = Toast.makeText(applicationContext,"Image Pressed",Toast.LENGTH_SHORT)
-        img.show()
-    }
-    fun checkStateOfCheckBox (){
-        val checkBox = findViewById<View>(android.R.id.checkbox) as CheckBox
-        val checkBoxState = checkBox.isChecked
-        print(checkBoxState)
+
+//    fun buttonPressed(v :View){
+//        println("button pressed from UI call")
+//        val btn = Toast.makeText(applicationContext,"Button Pressed",Toast.LENGTH_SHORT)
+//        btn.show();
+//        Snackbar.make(v,"Basic SnackBar",4000).setAction("Yes",View.OnClickListener {
+//            Toast.makeText(applicationContext,"Dismiss SnackBar",Toast.LENGTH_SHORT).show()
+//        }).show()
+//    }
+//    fun imagePressed(v :View){
+//        println("image pressed from UI call")
+//        val img = Toast.makeText(applicationContext,"Image Pressed",Toast.LENGTH_SHORT)
+//        img.show()
+//    }
+//    fun checkStateOfCheckBox (){
+//        val checkBox = findViewById<View>(android.R.id.checkbox) as CheckBox
+//        val checkBoxState = checkBox.isChecked
+//        print(checkBoxState)
+//    }
+}
+
+class Dice(val sides: Int) {
+    fun rollDice(): Int {
+        return (1..sides).random()
     }
 }
